cdb80cd0b5061825fae3f13324cbbb1674f6d0f5
Manuel934 <183951903+Manuelillo-dev@users.noreply.github.com>
2025-05-10 15:12:01 -0600

    Cruds
    En gestión

--- database/firebase.js
+++ database/firebase.js
@@ -2,13 +2,12 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBJqKyzEvnQFKQc-y4K31zbgj9rssXHz4Y",
-  authDomain: "react-native-97bfa.firebaseapp.com",
-  databaseURL: "https://react-native-97bfa.firebaseio.com",
-  projectId: "react-native-97bfa",
-  storageBucket: "react-native-97bfa.appspot.com",
-  messagingSenderId: "961930425654",
-  appId: "1:961930425654:web:cdbde3f2ff278ed214ef97",
+  apiKey: "AIzaSyCywgEiAyDNFCwMKjMZ27UH20vIw3PQU98",
+  authDomain: "appdemofirebase-e4868.firebaseapp.com",
+  projectId: "appdemofirebase-e4868",
+  storageBucket: "appdemofirebase-e4868.firebasestorage.app",
+  messagingSenderId: "637832033939",
+  appId: "1:637832033939:web:fc342f34b9ff6b209dbf66",
 };
 
 // Initialize Firebase
